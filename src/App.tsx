0a27254bb2966572ce6fd72cceef9a7da273289c
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-08-21 12:19:22 +0600

    added: test component button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
     <h1 className='text-red-50'>this is tailwind css installation</h1>
+    <Button>Shadcn</Button>
     </>
   )
 }
