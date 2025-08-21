ca83c50dfa7b0f7f6b9a5de6b87da9828ee675f6
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-08-21 18:24:13 +0600

    added: layout

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { RiTaskFill } from "react-icons/ri";
+import { Link } from 'react-router';
 
 const Navbar = () => {
     return (
-        <nav className='max-w-7xl mx-auto h-16 flex items-center gap-3 px-5 border'>
+        <nav className='max-w-7xl mx-auto h-16 flex justify-between items-center gap-3 px-5 border'>
+            {/* left side logo */}
             <div className='flex items-center'>
-                < RiTaskFill size={40}/>
+                < RiTaskFill size={40} />
                 <h1>Task Manager</h1>
+            </div>
+            {/* middle side nav items */}
+            <div>
+                <Link to="/tasks">Tasks</Link>
+
+            </div>
+
+            {/* right side items */}
+            <div>
+                <Link to="/tasks">Login</Link>
             </div>
         </nav>
     )
